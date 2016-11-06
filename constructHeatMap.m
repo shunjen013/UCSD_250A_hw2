d5eeda383e837a16176da016b4bfb9cbd546a35b
Eric Wen <eric199.wen@gmail.com>
2016-11-05 17:33:21 -0700

    Eric finished problem 4

--- constructHeatMap.m
+++ constructHeatMap.m
@@ -1,4 +1,6 @@
 function [ heat_map ] = constructHeatMap( input_img, filter_img )
+    filter_img = flip(filter_img, 1);
+    filter_img = flip(filter_img, 2);
     heat_map = conv2(input_img, filter_img, 'same');
     figure;
     imagesc(heat_map);
